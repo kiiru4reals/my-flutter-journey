06edb20c4231e708f36aa5c94e492baba6692ff0
kiiru4reals <smaina991@gmail.com>
2021-06-28 16:36:20 +0300

    Fully functional tipster app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_journey/ui/home.dart';
-import 'package:my_flutter_journey/ui/bizcard.dart';
+import 'ui/home.dart';
+import 'ui/bizcard.dart';
+import 'ui/tipster.dart';
 
 // void main() => runApp(ScaffoldExample());
 void main() => runApp( MaterialApp(
-  home: BizCard(),
+  home: Tipster(),
 ));
 
