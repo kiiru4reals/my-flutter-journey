dba1fc50a68ca25485bf27ef3a646052c40029a4
kiiru4reals <smaina991@gmail.com>
2021-07-07 12:09:26 +0300

    imported movies.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'ui/home.dart';
 import 'ui/bizcard.dart';
 import 'ui/tipster.dart';
 import 'ui/learn.dart';
+import 'ui/movies.dart';
 
 // void main() => runApp(ScaffoldExample());
 void main() => runApp( MaterialApp(
-  home: Quiz(),
+  home: Movies(),
 ));
 
